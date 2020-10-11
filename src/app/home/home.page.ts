02e44d7d19c4672fea782d09e89ca47471105460
Leandro <leandro.sorbo@gmail.com>
2020-10-11 10:51:59 -0300

    Add seletor de local de retirada e devolução

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-    constructor() {}
+    constructor(public actionSheetController: ActionSheetController) {}
   
-    
+    async selectLocation() {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Locais',
+        cssClass: 'my-custom-class',
+        buttons: [{
+          text: 'Shopping Aricanduva',
+          icon: 'location',
+          handler: () => {
+            console.log('Shopping Aricanduva');
+          }
+        }, {
+          text: 'Shopping Mooca',
+          icon: 'location',
+          handler: () => {
+            console.log('Shopping Mooca');
+          }
+        }, {
+          text: 'Shopping Pátio Higienólis',
+          icon: 'location',
+          handler: () => {
+            console.log('Shopping Pátio Higienólopis');
+          }
+        }, {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        }]
+      });
+      await actionSheet.present();
+    }
+  
   }
