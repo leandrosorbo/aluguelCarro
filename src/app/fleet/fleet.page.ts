f40908e7c43332a6f9237d94ed62b40d60a93936
Leandro <leandro.sorbo@gmail.com>
2020-10-11 10:43:07 -0300

    Criando home e unindo as branch

---
+++ src/app/fleet/fleet.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fleet',
+  templateUrl: './fleet.page.html',
+  styleUrls: ['./fleet.page.scss'],
+})
+export class FleetPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
