2dcaf36855effac50ce346fab263032da9a8f401
heell13 <hpalenca13@gmail.com>
2020-10-13 23:29:46 -0300

    identação

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular'; 
 
 
 @Component({
