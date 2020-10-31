13762dc5f1c024d88265a6ce4c1048046c17d9ff
Leandro <leandro.sorbo@gmail.com>
2020-10-31 11:15:25 -0300

    Implementação do seletor de datas retirada/devolução

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,40 +8,37 @@ import { ActionSheetController } from '@ionic/angular';
 })
 export class HomePage {
 
-    constructor(public actionSheetController: ActionSheetController) {}
+  selectedDateString1 = new Date().toISOString();
+  minDateInput: string = new Date().toISOString();
+  maxDateInput: string = new Date().toISOString();
+
+  selectedDateString2 = new Date().toISOString();
+  minDateOutput: string = new Date().toISOString();
+  maxDateOutput: string = new Date().toISOString();
+
+  constructor(private platform1: Platform, private platform2: Platform) {
   
-    async selectLocation() {
-      const actionSheet = await this.actionSheetController.create({
-        header: 'Locais',
-        cssClass: 'my-custom-class',
-        buttons: [{
-          text: 'Shopping Aricanduva',
-          icon: 'location',
-          handler: () => {
-            console.log('Shopping Aricanduva');
-          }
-        }, {
-          text: 'Shopping Mooca',
-          icon: 'location',
-          handler: () => {
-            console.log('Shopping Mooca');
-          }
-        }, {
-          text: 'Shopping Pátio Higienólis',
-          icon: 'location',
-          handler: () => {
-            console.log('Shopping Pátio Higienólopis');
-          }
-        }, {
-          text: 'Cancelar',
-          icon: 'close',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancelar');
-          }
-        }]
-      });
-      await actionSheet.present();
-    }
-  
+
+    this.platform1.ready().then(() => {
+      let date1: Date = new Date();
+      date1.setDate(date1.getDate() - 0);
+      this.minDateInput = date1.toISOString();
+
+      date1 = new Date();
+      date1.setDate(date1.getDate() + 365);
+      this.maxDateInput = date1.toISOString();
+
+
+    });
+    this.platform2.ready().then(() => {
+      let date2: Date = new Date();
+      date2.setDate(date2.getDate() - 0);
+      this.minDateInput = date2.toISOString();
+
+      date2 = new Date();
+      date2.setDate(date2.getDate() + 365);
+      this.maxDateOutput = date2.toISOString();
+
+    })
   }
+}
